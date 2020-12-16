0412043eeb27b6626f34dc9f670458cc11889da9
eyujunior <eyujunior052@gmail.com>
2020-12-16 15:26:14 -0800

    added scroll-triggered animations and added live demo

--- app.js
+++ app.js
@@ -39,3 +39,38 @@ document.querySelectorAll('.btn').forEach(element => {
     console.log('clicked');
   })
 });
+
+gsap.from('.col-2', {opacity:0, x:-200, duration: 1})
+gsap.from('.hero-img', {opacity:0, x:200, duration: 1})
+gsap.from('.header', {opacity:0, y:50, duration: 1})
+
+window.sr = ScrollReveal();
+
+
+sr.reveal('.animate-left', {
+    origin: 'left',
+    duration: 1000,
+    distance: '25rem',
+    delay: 300
+});
+
+sr.reveal('.animate-right', {
+    origin: 'right',
+    duration: 1000,
+    distance: '25rem',
+    delay: 300
+});
+
+sr.reveal('.animate-top', {
+    origin: 'top',
+    duration: 1000,
+    distance: '25rem',
+    delay: 400
+});
+
+sr.reveal('.animate-bottom', {
+    origin: 'bottom',
+    duration: 1000,
+    distance: '25rem',
+    delay: 400
+});
